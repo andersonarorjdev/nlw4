1586149aa87c954ee17c7dc4fcda7ac72dbaba28
andersonarorjdev <anderson.r.o.r.jr@gmail.com>
2021-02-22 08:59:59 -0300

    Initial commit #nlw4

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Partiu Mais uma nlw
         </a>
       </header>
     </div>
